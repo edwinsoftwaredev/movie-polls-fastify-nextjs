3f97a2bd10021c4f285d01a73a3ed461e74586de
edwinsoftwaredev <edwin_m_paz@hotmail.com>
2022-10-10 15:34:03 -0700

    update comments

--- api/types/fastify/index.d.ts
+++ api/types/fastify/index.d.ts
@@ -4,10 +4,13 @@ import { OAuth2Client, LoginTicket } from 'google-auth-library';
 
 // Importing these type declaration allows the LSP to
 // provide methods and properties from them.
+// (type/module/global module augmentation)
 // TODO: validate that importing these types
 // does not affect builds
 //
 // https://www.typescriptlang.org/docs/handbook/release-notes/typescript-3-8.html
+// https://www.fastify.io/docs/latest/Reference/TypeScript/#using-a-plugin
+// https://www.typescriptlang.org/docs/handbook/declaration-merging.html#module-augmentation
 import type * as FastifySession from '@fastify/session';
 import type * as FastifyCsrf from '@fastify/csrf-protection';
 import type * as Fastify from 'fastify';
--- trpc/server/routers/account.ts
+++ trpc/server/routers/account.ts
@@ -8,9 +8,6 @@ export const account = createRouter().query('whoami', {
 
     if (!userId) return { whoami: null };
 
-    // TODO: type should not be defined but inferrered.
-    // If the type is defined, on client side, the return type
-    // will be any
     const whoami = await fastify.account.user.getUser(userId);
 
     return { whoami };
