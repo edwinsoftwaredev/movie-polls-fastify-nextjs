60b4b50e4f30da208e697d9c0dc5bca76f7aeefe
edwinsoftwaredev <edwin_m_paz@hotmail.com>
2022-10-14 15:06:50 -0700

    add initial security headers to web-client

--- web-client/next.config.js
+++ web-client/next.config.js
@@ -1,7 +1,64 @@
+// https://nextjs.org/docs/advanced-features/security-headers
+// https://helmetjs.github.io/
+
+const apiURL = process.env.NEXT_PUBLIC_API_HOST_URL || '';
+// https://cloud.google.com/identity-platform/docs/web/chrome-extension
+const googleURLs = 'https://accounts.google.com'; 
+
+// TODO: review headers in prod
+const ContentSecurityPolicy = `
+  base-uri 'self';
+  default-src 'self';
+  script-src 'self' ${googleURLs} 'unsafe-eval';
+  connect-src 'self' ${apiURL};
+  child-src 'self';
+  frame-src 'self' ${googleURLs};
+  frame-ancestors 'self';
+  style-src 'self' ${googleURLs} 'unsafe-inline';
+  font-src 'self';
+  img-src 'self' data:;
+  form-action 'self' ${apiURL};
+  object-src 'none';
+  script-src-attr 'none';
+  upgrade-insecure-requests 
+`;
+
+const securityHeaders = [{
+  key: 'Content-Security-Policy',
+  value: ContentSecurityPolicy.replace(/\s{2,}/g, ' ').trim()
+}, {
+  key: 'X-Content-Type-Options',
+  value: 'nosniff'
+}, {
+  key: 'Referrer-Policy',
+  value: 'strict-origin-when-cross-origin'
+},{
+  key: 'Cross-Origin-Opener-Policy',
+  value: 'same-origin-allow-popups' // allow-popups will allow the Google Sign In With Pop Up to render
+}, {
+  key: 'Origin-Agent-Cluster',
+  value: '?1'
+}, {
+  key: 'X-Frame-Options',
+  value: 'SAMEORIGIN'
+}, {
+  key: 'X-Permitted-Cross-Domain-Policies',
+  value: 'none'
+}, {
+  key: 'Strict-Transport-Security',
+  value: 'max-age=15552000; includeSubDomains'
+}];
+
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
+  async headers() {
+    return [{
+      source: '/:path*',
+      headers: securityHeaders
+    }]
+  }
 };
 
 module.exports = nextConfig;
