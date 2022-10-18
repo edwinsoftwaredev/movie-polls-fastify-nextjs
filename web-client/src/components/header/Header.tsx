0a955459a768bfb7feadd9c0379e337c4069d9f8
edwinsoftwaredev <edwin_m_paz@hotmail.com>
2022-10-17 18:50:40 -0700

    add links to Header nav component

--- web-client/src/components/header/Header.tsx
+++ web-client/src/components/header/Header.tsx
@@ -1,9 +1,93 @@
+import Link from 'next/link';
+import { useEffect, useRef } from 'react';
+import trpc from 'src/trpc';
+import style from './Header.module.scss';
+
+const AnonymousUserNavOptions: React.FC = () => (
+  <ul>
+    <li>
+      <Link href='/about'>
+        <a>About</a>
+      </Link>
+    </li>
+    <li>
+      <Link href='/contact'>
+        <a>Contact</a>
+      </Link>
+    </li>
+    <hr />
+    <li>
+      <Link href='/signin'>
+        <a>Sign In</a>
+      </Link>
+    </li>
+  </ul>
+);
+
+const NavOptions: React.FC = () => (
+  <ul>
+    <li>
+      <Link href='/search'>
+        <a>Search</a>
+      </Link>
+    </li>
+    <li>
+      <Link href='/home'>
+        <a>Home</a>
+      </Link>
+    </li>
+    <li>
+      <Link href='/top-movies'>
+        <a>Top Movies</a>
+      </Link>
+    </li>
+    <li>
+      <Link href='/trending-movies'>
+        <a>Trending Movies</a>
+      </Link>
+    </li>
+    <li>
+      <Link href='/my-polls'>
+        <a>My Polls</a>
+      </Link>
+    </li>
+
+    <hr />
+
+    <li>
+      <Link href='/account'>
+        <a>Account</a>
+      </Link>
+    </li>
+    <li>
+      <Link href='/signout'>
+        <a>Sign Out</a>
+      </Link>
+    </li>
+  </ul>
+);
+
 const Header: React.FC = () => {
+  const { data: sessionData} = trpc.useQuery(['session:getSession'], {
+    refetchOnMount: false,
+  });
+
+  const { isAuthenticated } = sessionData || {};
+
+
   return (
-    <header>
-      <div><h2>Movie Polls</h2></div>
-    </header>
+    <div className={style['container']}>
+      <header className={style['header']}>
+        <h1>Movie Polls</h1>
+        <div />
+        <nav>
+          {
+            isAuthenticated ? <NavOptions /> : <AnonymousUserNavOptions />
+          }
+        </nav>
+      </header>
+    </div>
   )
-}
+};
 
 export default Header;
