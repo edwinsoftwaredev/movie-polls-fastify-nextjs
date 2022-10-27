528ca2616d665c0e0301a20ee5e13397d39df680
edwinsoftwaredev <edwin_m_paz@hotmail.com>
2022-10-27 15:07:58 -0700

    enable prisma referential integrity

--- prisma/migrations/20221027220330_init_migration/migration.sql
+++ prisma/migrations/20221027220330_init_migration/migration.sql
@@ -11,13 +11,11 @@ CREATE TABLE `User` (
 ) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
 
 -- CreateTable
-CREATE TABLE `Session` (
+CREATE TABLE `UserSession` (
     `id` VARCHAR(191) NOT NULL,
     `userId` VARCHAR(191) NULL,
     `csrfToken` VARCHAR(191) NOT NULL,
+    `expiresOn` DATETIME(3) NULL,
 
     PRIMARY KEY (`id`)
 ) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
-
--- AddForeignKey
-ALTER TABLE `Session` ADD CONSTRAINT `Session_userId_fkey` FOREIGN KEY (`userId`) REFERENCES `User`(`id`) ON DELETE SET NULL ON UPDATE CASCADE;
