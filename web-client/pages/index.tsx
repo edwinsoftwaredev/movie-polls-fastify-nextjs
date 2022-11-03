b693ffa25c3272a4743a71275ebd51d165eef74b
edwinsoftwaredev <edwin_m_paz@hotmail.com>
2022-11-02 18:29:04 -0700

    update trpc client

--- web-client/pages/index.tsx
+++ web-client/pages/index.tsx
@@ -9,7 +9,6 @@ export const getServerSideProps: GetServerSideProps = async ({ req }) => {
   const trpcClient = getTRPCClient({ req });
   const queryClient = new QueryClient();
   const whoamiQueryData = await trpcClient.query('account:whoami').catch(err => {
-    console.log('index.ts', err);
     return { whoami: null };
   });
   queryClient.setQueryData('account:whoami', whoamiQueryData);
