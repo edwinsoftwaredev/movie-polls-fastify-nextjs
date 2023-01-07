0b5ce996b0009e0de142d7863f703a34bc742736
edwinsoftwaredev <edwin_m_paz@hotmail.com>
2023-01-06 17:51:31 -0800

    add ip-rate-limit middleware

---
+++ api/api/middleware.ts
@@ -0,0 +1,16 @@
+import { next } from '@vercel/edge';
+import { ipRateLimit } from './vercel/ip-rate-limt';
+
+export const config = {
+  matcher: ['/publicMoviesRoutes/:path*'],
+  runtime: 'edge',
+  regions: ['iad1'], // only execute this function on iad1
+};
+
+export default async (req: Request) => {
+  const ipRateLimitRes = await ipRateLimit(req);
+  // If the status is not 200 then it has been rate limited.
+  if (ipRateLimitRes.status !== 200) return ipRateLimitRes;
+
+  return next();
+};
