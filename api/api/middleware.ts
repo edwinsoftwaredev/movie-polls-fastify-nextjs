e042678da5f5d0a30830a8d0efd98fdc33d914cf
edwinsoftwaredev <edwin_m_paz@hotmail.com>
2023-01-09 20:14:39 -0800

    update middleware.ts

--- api/api/middleware.ts
+++ api/api/middleware.ts
@@ -1,19 +1,26 @@
-import { next } from '@vercel/edge';
-import { ipRateLimit } from './vercel/ip-rate-limt';
+import { next, rewrite } from '@vercel/edge';
+import { ipRateLimit } from '../lib/ip-rate-limit';
 
 export const config = {
-  matcher: ['/trpc/publicMoviesRoutes/:path*'],
+  // matcher: ['/trpc/publicMoviesRoutes/:path*'],
   runtime: 'edge',
-  regions: ['iad1'], // only execute this function on iad1
+  // regions: ['iad1'], // only execute this function on iad1
 };
 
 export default async (req: Request) => {
-  // TODO: consider device fingerprinting
-  const ipRateLimitRes = await ipRateLimit(req);
-  // If the status is not 200 then it has been rate limited.
-  if (ipRateLimitRes.status !== 200) return ipRateLimitRes;
+  const url = new URL(req.url);
+
+  console.log(url);
+
+  if (url.pathname.startsWith('/trpc/publicMoviesRoutes/')) {
+    // TODO: consider device fingerprinting
+    const ipRateLimitRes = await ipRateLimit(req);
+
+    // If the status is not 200 then it has been rate limited.
+    if (ipRateLimitRes.status !== 200) return ipRateLimitRes;
+  }
 
   return next({
-    headers: { 'x-from-middleware': 'true' },
+    headers: { 'x-from-middleware': '1' },
   });
 };
